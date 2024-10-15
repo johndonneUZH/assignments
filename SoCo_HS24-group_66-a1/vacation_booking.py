278ec42773c3bb3e0481cf3d7bd9dc3fc309200f
johndonneUZH <matteoiulian.adam@uzh.ch>
2024-10-15 23:14:10 +0200

    defensive programming

--- SoCo_HS24-group_66-a1/vacation_booking.py
+++ SoCo_HS24-group_66-a1/vacation_booking.py
@@ -1,3 +1,11 @@
+import re
+from collections import defaultdict
+# Authors: SoCo_HS24-group_66-a1
+
+# --------------------------------------------------------------------
+# Parent class: VACATION PACKAGE
+# --------------------------------------------------------------------
+
 # Abstract methods for subclasses to implement
 def calculate_cost(vacation_package: dict):
     return NotImplementedError("Abstract Class: This method must be implemented by a subclass")
@@ -40,6 +48,8 @@ def describe_package_BeachResort(vacation_package: dict):
     description = description.replace("vacation", vacation_package["_class"]["_description"])
     if vacation_package['includes_surfing']:
         description += " includes surfing"
+    else:
+        description += " does not include surfing"
     return description
 
 BeachResort = {
@@ -131,8 +141,14 @@ def do_LuxuryCruise(destination: str, cost_per_day: int, duration_in_days: int, 
 # --------------------------------------------------------------------
 # UTILITY FUNCTIONS
 # --------------------------------------------------------------------
-
 def call(vacation_package: dict, method_name: str):
+    # Defensive programming
+    if not isinstance(vacation_package, dict):
+        raise ValueError("Invalid vacation package")
+    
+    if not isinstance(method_name, str):
+        raise ValueError("Invalid method name")
+    
     try:
         method = find_method(vacation_package['_class'], method_name)
         return method(vacation_package)
@@ -149,6 +165,16 @@ def find_method(cls: dict, method_name: str):
 def make(vacation_class: dict, destination: str, cost_per_day: int, duration_in_days: int, *args):
     dict_packages = {name.strip('do_'): func for name, func in globals().items() if name.startswith('do_')}
     
+    # Defensive programming
+    if not isinstance(vacation_class, dict):
+        raise ValueError("Invalid vacation package class")
+    if not isinstance(destination, str):
+        raise ValueError("Invalid destination")
+    if not isinstance(cost_per_day, int) or cost_per_day < 0:
+        raise ValueError("Invalid cost per day")
+    if not isinstance(duration_in_days, int) or duration_in_days < 0:
+        raise ValueError("Invalid duration in days")
+
     # Get the vacation type
     vacation_type = vacation_class['_classname']
 
@@ -161,24 +187,44 @@ def make(vacation_class: dict, destination: str, cost_per_day: int, duration_in_
         raise ValueError(f"Invalid vacation package type: {vacation_type}")
     
     # Bools includes_surfing or has_private_suit are not passed into make()
-    if vacation_type == "BeachResort" or vacation_type == "LuxuryCruise":
-        if len(args) != 1 or not isinstance(args[0], bool):
-            raise ValueError(f"{vacation_type} requires 1 additional argument")
-        return constructor_func(destination, cost_per_day, duration_in_days, args[0])
+    match vacation_type:
+        case "BeachResort" | "LuxuryCruise":
+            if len(args) != 1 or not isinstance(args[0], bool):
+                raise ValueError(f"{vacation_type} requires only 1 additional argument")
+        case "AdventureTrip":
+            if len(args) != 1:
+                raise ValueError(f"{vacation_type} requires 1 additional argument")
+            if args[0] not in ("easy", "hard")  or not isinstance(args[0], str):
+                raise ValueError(f"{vacation_type} is either easy or hard")
+        case _:
+            pass
 
-    if vacation_type == "AdventureTrip":
-        if len(args) != 1:
-            raise ValueError(f"{vacation_type} requires 1 additional argument")
-        if args[0] not in ("easy", "hard")  or not isinstance(args[0], str):
-            raise ValueError(f"{vacation_type} is either easy or hard")
-        return constructor_func(destination, cost_per_day, duration_in_days, args[0])
-    return constructor_func(destination, cost_per_day, duration_in_days)
+    return constructor_func(destination, cost_per_day, duration_in_days, *args)
+
 
 # --------------------------------------------------------------------
+# GLOBAL VARIABLES
 # --------------------------------------------------------------------
+vacation_summaries = {name.strip('do_'): [] for name, func in globals().items() if name.startswith('do_')}
+
+def add_to_vacation_summaries(vacation_type: str, destination: str, cost_per_day: int, duration_in_days: int, *args):
+    global vacation_summaries
+    if vacation_type in vacation_summaries:
+        vacation_summaries[vacation_type].append(
+            {
+                "destination": destination,
+                "cost_per_day": cost_per_day,
+                "duration_in_days": duration_in_days,
+                "args": list(args)
+            }
+        )
+
+# --------------------------------------------------------------------
+# MAIN
 # --------------------------------------------------------------------
 
 def main():
+    print(vacation_summaries)
     beach_resort = make(BeachResort, "Maldives", 100, 7, True)
     print(call(beach_resort, "describe_package"))
     print(call(beach_resort, "calculate_cost"))
@@ -186,13 +232,12 @@ def main():
     adventure_trip = make(AdventureTrip, "Macchu Picchu", 50, 8, "hard")
     print(call(adventure_trip, "describe_package"))
     print(call(adventure_trip, "calculate_cost"))
-    
-    luxury_cruise = make(LuxuryCruise, "Malta", 200, 14, False)
+
+    luxury_cruise = make(LuxuryCruise, "Malta", 200, 14, True)
     print(call(luxury_cruise, "describe_package"))
     print(call(luxury_cruise, "calculate_cost"))
 
-
-
+    print(vacation_summaries)
 
 
 if __name__ == "__main__":
