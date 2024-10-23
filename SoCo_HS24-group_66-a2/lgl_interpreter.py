c561c1f19e09d775052dd2cf135add948417431b
Vite <jmiguel.vite@gmail.com>
2024-10-23 23:20:48 +0200

    First changes to lgl_interpreter.py

--- SoCo_HS24-group_66-a2/lgl_interpreter.py
+++ SoCo_HS24-group_66-a2/lgl_interpreter.py
@@ -10,6 +10,58 @@ def program_reader():
 def evaluate_expression(expr, env):
     pass
 
+def do_addieren(env, args):
+    assert len(args) == 2
+    left = do(env, args[0])
+    right = do(env, args[1])
+    return left + right
+def do_subtrahieren(env, args):
+    assert len(args) == 2
+    left = do(env, args[0])
+    right = do(env, args[1])
+    return left - right
+
+def do_multiplizieren(env, args):
+    assert len(args) == 2
+    left = do(env, args[0])
+    right = do(env, args[1])
+    return left * right
+
+def do_dividieren(env, args):
+    assert len(args) == 2
+    left = do(env, args[0])
+    right = do(env, args[1])
+    assert right != 0, "Error: división por cero"
+    return left / right
+
+def do_betrag(env, args):
+    assert len(args) == 1
+    val = do(env, args[0])
+    return abs(val)
+
+def do_setzen(env, args):
+    assert len(args) == 2
+    assert isinstance(args[0], str)
+    var_name = args[0]
+    value = do(env, args[1])
+    env[var_name] = value
+    return value
+
+def do_bekommen(env, args):
+    assert len(args) == 1
+    assert isinstance(args[0], str)
+    assert args[0] in env, f"Variable name {args[0]} not found"
+    value = env[args[0]]
+    return value
+
+def do_sequenz(env, args):
+    assert len(args) > 0
+    result = None
+    for expr in args:
+        result = do(env, expr)
+    return result
+
+
 OPERATIONS =  {
     "addieren": do_addieren,
     "substraieren": do_subtrahieren,
@@ -20,7 +72,6 @@ OPERATIONS =  {
     "bekommen": do_bekommen,
     "sequenz": do_sequenz
 }
-
     
 def do(env, expr):
     if isinstance(expr, list):
